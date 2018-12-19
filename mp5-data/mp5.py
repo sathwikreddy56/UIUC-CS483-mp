c05b448fa03e5af3518a42c98a64ccc32b147925
alexfanchina <alexfanchina@gmail.com>
2018-12-19 13:23:35 -0600

    update gitignore

--- mp5-data/mp5.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-data = np.loadtxt('3/input.raw')[1:]
-scan = [sum(data[:i + 1]) for i in range(len(data))]
-print(data, 'len=%d' % len(data))
-for i in range(len(scan)):
-	print('scan[%d] = %.1f' % (i, scan[i]))
